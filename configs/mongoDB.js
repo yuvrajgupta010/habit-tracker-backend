5714eb72334532f272d3427e2d9ddec04400b3ce
Yuvraj Gupta <Yuvrajgupta010@gmail.com>
2024-10-21 13:11:54 +0530

    mongodb string change

--- configs/mongoDB.js
+++ configs/mongoDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-// const MONGODB_URI = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@habit-tracker.sulnj.mongodb.net/?retryWrites=true&w=majority&appName=habit-tracker`;
-const MONGODB_URI = `mongodb://localhost:27017/habit-tracker`;
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@habit-tracker.sulnj.mongodb.net/?retryWrites=true&w=majority&appName=habit-tracker`;
+// const MONGODB_URI = `mongodb://localhost:27017/habit-tracker`;
 
 const mongoDBConnection = mongoose
   .connect(MONGODB_URI)
